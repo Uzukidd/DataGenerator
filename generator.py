2e7624179e016f8782bf5cd1474c89bc30986fc6
uzukidd <uzukidd@gmail.com>
2025-10-31 15:08:41 +0800

    feat: add filter empty scene

--- generator.py
+++ generator.py
@@ -5,6 +5,27 @@ from SynchronyModel import SynchronyModel
 from config import cfg_from_yaml_file
 from data_utils import objects_filter, merge_cyclist_label, merge_car_label, reverse_rotation, kitti_label_filter
 
+def filter_empty_scene():
+    import os
+
+    folder = "data/test/training/kitti_label"
+    output_file = os.path.join("data/test/training", "filtered_train.txt")
+
+    txt_files = [f for f in os.listdir(folder) if f.endswith(".txt")]
+
+    non_empty_files = [f for f in txt_files if os.path.getsize(os.path.join(folder, f)) > 0]
+
+    names = [os.path.splitext(f)[0] for f in non_empty_files]
+
+    names.sort()
+
+    with open(output_file, "w", encoding="utf-8") as outfile:
+        for name in names:
+            outfile.write(name + "\n")
+
+    print(f"共找到 {len(names)} 个非空文件，结果已保存到：{output_file}")
+
+
 def main():
     cfg = cfg_from_yaml_file("configs.yaml")
     model = SynchronyModel(cfg)
@@ -44,6 +65,7 @@ def main():
     finally:
         model.setting_recover()
 
+    filter_empty_scene()
 
 if __name__ == '__main__':
     main()
